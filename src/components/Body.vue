7c71dd271bb1a2c9b40cb3fb2846918993215747
Bisio98 <alberto.bisetti@hotmail.it>
2022-01-12 12:57:49 +0100

    milestone-2

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,15 +1,32 @@
 <template>
     <div>
         <input v-model="searched" type="text">
-        <button @click="searchInApi">Cerca</button>
-        <ul>
-            <li v-for="(element,index) in found" :key="index">
-                <div>{{ element.title }}</div>
-                <div>{{ element.original_title }}</div>
-                <div>{{ element.original_language }}</div>
-                <div>{{ element.vote_average }}</div>
-            </li>
-        </ul>
+        <button @click="searchFilmsInApi(); searchSeriesInApi();">Cerca</button>
+        <div class="list">
+            <h2>FILMS</h2>
+            <ul>
+                <li v-for="(element,index) in foundFilms" :key="index">
+                    <div>{{ element.title }}</div>
+                    <div>{{ element.original_title }}</div>
+                    <div><img :src="getFlag(element.original_language)" alt=""></div>
+                    <div>{{ element.vote_average }}</div>
+                </li>
+            </ul>
+        
+            <h2>TV SERIES</h2>
+            <ul>
+                <li v-for="(element,index) in foundTv" :key="index">
+                    <div>{{ element.name }}</div>
+                    <div>{{ element.original_name }}</div>
+                    <div>
+                        <img v-if="getFlag(element.original_language)" :src="getFlag(element.original_language)" alt="">
+                        <span v-else>{{ element.original_language }}</span>
+                    </div>
+                    <div>{{ element.vote_average }}</div>
+                </li>
+            </ul>
+        </div>
+        
     </div>
 </template>
 
@@ -21,25 +38,67 @@ export default {
     data: function(){
         return {
             searched: '',
-            found: []
+            foundFilms: [],
+            foundTv: [],
+            flag: ''
         }
     },
     methods: {
-        searchInApi: function(){
+
+        searchFilmsInApi: function(){
             axios.get('https://api.themoviedb.org/3/search/movie',{
                 params: {
                     api_key: '69739f2b59204dc50fc56c7466acc9d8',
                     query: this.searched
                 }
             }).then((response) => {
-                this.found = response.data.results;
-                console.log(this.found);
+                this.foundFilms = response.data.results;
+                console.log(this.foundFilms);
             }
-        )}
+        )},
+        searchSeriesInApi: function(){
+            axios.get('https://api.themoviedb.org/3/search/tv',{
+                params: {
+                    api_key: '69739f2b59204dc50fc56c7466acc9d8',
+                    query: this.searched
+                }
+            }).then((response) => {
+                this.foundTv = response.data.results;
+                console.log(this.foundTv);
+            }
+        )},
+        getFlag: function(flagInput){
+            if(flagInput === 'en')
+                return 'https://countryflagsapi.com/svg/' + 'usa';
+            else if(flagInput === 'ja')
+                return 'https://countryflagsapi.com/svg/' + 'jp';
+            else{
+                return 'https://countryflagsapi.com/svg/' + flagInput;
+            }
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+    img{
+        width: 100px;
+    }
 
+    .list{
+        display: flex;
+    }
+
+    ul{
+        list-style: none;
+        
+        li{
+            display: flex;
+            flex-direction: row;
+
+            div{
+                margin: 10px;
+            }
+        }
+    }
 </style>
